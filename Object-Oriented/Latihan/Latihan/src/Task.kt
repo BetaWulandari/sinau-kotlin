e3d47d393ee74900f4a42b272c5d044fb3a26de9
Beta Wulandari <betawulandari21@gmail.com>
2020-06-05 11:04:57 +0700

    sinau kotlin

--- Object-Oriented/Latihan/Latihan/src/Task.kt
+++ Object-Oriented/Latihan/Latihan/src/Task.kt
@@ -6,47 +6,34 @@ import kotlin.reflect.KProperty
  * Cukup tambahkan kode berdasarkan perintah yang sudah ditentukan.
  *
  */
-class tidur{
-    private var value: String = "Default"
-    operator fun getValue(Cat: Any?, property: KProperty<*>) : String {
-      //  println("Fungsi ini sama seperti getter untuk properti ${property.name} pada class $classRef")
-        println("setter")
-        return value
-    }
-
-    operator fun setValue(Cat: Any?, property: KProperty<*>, newValue: String){
-      //  println("Fungsi ini sama seperti setter untuk properti ${property.name} pada class $classRef")
-     //   println("Nilai ${property.name} dari: $value akan berubah menjadi $newValue")
-        println("setter")
-        value = newValue
-    }
-
-}
-
-
 
 class Cat (private val name: String) {
     // TODO 1
-
     var sleep: Boolean = false
 
+        get(){
+            println("Fungsi getter dipanggil")
+            return field
+        }
+        set(value){
+            println("Fungsi setter dipanggil")
+            field = value
+        }
 
     fun toSleep() {
-
-        if (sleep == true){
+        if(sleep){
             println("$name, sleep!")
         } else {
-            println("$name, let's play")
+            println("$name, let's play!")
         }
+
     }
 }
 
 fun main() {
 
     // TODO 2
-
     val gippy = Cat("Gippy")
-
 
     gippy.toSleep()
     gippy.sleep = true
